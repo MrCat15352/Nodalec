0eda3153fa65efe6f67958a607078f8e8d16aedb
Ganza9991 <ganza9991@gmail.com>
2025-04-15 02:23:29 +0300

    build.js

--- tools/build/build.js
+++ tools/build/build.js
@@ -223,6 +223,7 @@ export const DmTarget = new Juke.Target({
     'interface/**',
     'sound/**',
     'modular_nova/**', ///NOVA EDIT ADDITION - Making the CBT work
+	  'nodalec/**', ///NODALECT EDIT ADDITION
     `${DME_NAME}.dme`,
     NamedVersionFile,
   ],
