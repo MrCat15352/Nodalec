Burbonchik/Guns-Nodalec

аванпост(чтоб посмотрели) (#21)
* мяу

* мяу

* аванпост что-то там

* мяу

* Добавил пустую строчку

* аванпост что-то там чать 2(два)

* фиксы

* мур

* Добавил фракцию датум

* Сделал небольшую структуру для модульности Нодалека

* Расширил структуру нодалека. Сделал отдельные хелперы и дефайны и встроил в модуль

* Починил пути

* Добавил сабсистемы faction частично

* Еще починил пути к атласам

* Ещё пути

* Исправлены пути и добавлены атласы

* Опять пути к атласам...

* А это уже пути к звукам. Замена путей

* Починил звуки лифта

* Да вы заебали со своими атласами

* Поправил структуру. Вынес мод Дахкси в отдельную папку вне ядра переноса. 49 ошибок осталось

* Пофиксил косяк при создании модульности

* Фикс комментария

* Заменил листы армора на датумы "armor_type = /datum/armor/immune"

* Убрал лишний аргумент. Закомментировал заглушку на оверайд LateInitialize

* Удалил дубликат кода элеватора. Осталось 21 ошибка

* Исправил incapacitated() на incapacitated

* много штук

* Добавил ошибки, убрал исправления

* продукт буквосодержащий

* скажи мяу

* Добавлены дефайны

* Добавлены еще дефайны карты, и на будущее тоже

* Добавлен файл с настройками игры

* Исправлен путь до карт с аванпостами

* Добавлены переменные х и у

* Вроде как добавил в админ панельку спавн аванпоста, но чет нихуя

* Перелопатил проки, добавил не достающих проков (не все) для аванпоста

* Поправил ретурн, засунул под иф

---------

Co-authored-by: MrCat15352 <125268716+MrCat15352@users.noreply.github.com>

nodalec/master_files/modules/mob/living/simple_animal/hostile/mining_mobs/wolf.dm
@@ -1,0 +1,73 @@
+//alpha wolf- smaller chance to spawn, practically a miniboss. Has the ability to do a short, untelegraphed lunge with a stun. Be careful!
+/mob/living/simple_animal/hostile/asteroid/wolf/alpha
+	name = "alpha wolf"
+	desc = "An old wolf with matted, dirty fur and a missing eye, trophies of many won battles and successful hunts. Seems like they're the leader of the pack around here. Watch out for the lunge!"
+	icon = 'nodalec/master_files/icons/mob/icemoon/icemoon_monsters.dmi'
+	icon_state = "alphawolf"
+	icon_living = "alphawolf"
+	icon_dead = "alphawolf_dead"
+	speed = 15
+	move_to_delay = 15
+	vision_range = 4
+	aggro_vision_range = 12
+	maxHealth = 100
+	health = 100
+	melee_damage_lower = 10
+	melee_damage_upper = 10
+	dodging = TRUE
+	dodge_prob = 75
+	//charger = TRUE
+	//charge_distance = 7
+	//knockdown_time = 1 SECONDS
+	//charge_frequency = 20 SECONDS
+	//butcher_results = list(/obj/item/reagent_containers/food/snacks/meat/slab = 2, /obj/item/stack/sheet/sinew/wolf = 4, /obj/item/stack/sheet/sinew/wolf = 4, /obj/item/stack/sheet/bone = 5)
+	loot = list()
+	crusher_loot = /obj/item/crusher_trophy/fang
+
+/mob/living/simple_animal/hostile/asteroid/wolf/alpha/gib()
+	move_force = MOVE_FORCE_DEFAULT
+	move_resist = MOVE_RESIST_DEFAULT
+	pull_force = PULL_FORCE_DEFAULT
+	if(prob(75))
+		new /obj/item/crusher_trophy/fang(loc)
+		visible_message("<span class='warning'>You find an intact fang that looks salvagable.</span>")
+	..()
+
+/obj/item/crusher_trophy/fang
+	name = "battle-stained fang"
+	desc = "A wolf fang, displaying the wear and tear associated with a long and colorful life. Could be attached to a kinetic crusher or used to make a trophy."
+	icon = 'icons/obj/mining_zones/elite_trophies.dmi'
+	icon_state = "fang"
+	denied_type = /obj/item/crusher_trophy/fang
+	var/bleed_stacks_per_hit = 5
+
+/obj/item/crusher_trophy/fang/effect_desc()
+	return "waveform collapse to build up a small stack of bleeding, causing a burst of damage if applied repeatedly."
+
+/obj/item/crusher_trophy/fang/on_mark_detonation(mob/living/target, mob/living/user)
+	. = ..()
+	if(istype(target) && (target.mob_biotypes & MOB_ORGANIC))
+		var/datum/status_effect/stacking/saw_bleed/bloodletting/B = target.has_status_effect(/datum/status_effect/stacking/saw_bleed/bloodletting)
+		if(!B)
+			target.apply_status_effect(/datum/status_effect/stacking/saw_bleed/bloodletting, bleed_stacks_per_hit)
+		else
+			B.add_stacks(bleed_stacks_per_hit)
+
+/mob/living/simple_animal/hostile/asteroid/wolf/random/Initialize()
+	. = ..()
+	if(prob(15))
+		new /mob/living/simple_animal/hostile/asteroid/wolf/alpha(loc)
+		return INITIALIZE_HINT_QDEL
+
+/mob/living/simple_animal/hostile/asteroid/wolf/wasteland
+	faction = list(FACTION_WASTELAND)
+
+/mob/living/simple_animal/hostile/asteroid/wolf/alpha/wasteland
+	faction = list(FACTION_WASTELAND)
+
+/mob/living/simple_animal/hostile/asteroid/wolf/wasteland/random/Initialize()
+	. = ..()
+	if(prob(15))
+		new /mob/living/simple_animal/hostile/asteroid/wolf/alpha/wasteland(loc)
+		return INITIALIZE_HINT_QDEL
+

MrCat15352/Nodalec

nodalec/master_files/modules/mob/living/simple_animal/hostile/mining_mobs/wolf.dm
@@ -1,0 +1,72 @@
+//alpha wolf- smaller chance to spawn, practically a miniboss. Has the ability to do a short, untelegraphed lunge with a stun. Be careful!
+/mob/living/simple_animal/hostile/asteroid/wolf/alpha
+	name = "alpha wolf"
+	desc = "An old wolf with matted, dirty fur and a missing eye, trophies of many won battles and successful hunts. Seems like they're the leader of the pack around here. Watch out for the lunge!"
+	icon = 'nodalec/master_files/icons/mob/icemoon/icemoon_monsters.dmi'
+	icon_state = "alphawolf"
+	icon_living = "alphawolf"
+	icon_dead = "alphawolf_dead"
+	speed = 15
+	move_to_delay = 15
+	vision_range = 4
+	aggro_vision_range = 12
+	maxHealth = 100
+	health = 100
+	melee_damage_lower = 10
+	melee_damage_upper = 10
+	dodging = TRUE
+	dodge_prob = 75
+	//charger = TRUE
+	//charge_distance = 7
+	//knockdown_time = 1 SECONDS
+	//charge_frequency = 20 SECONDS
+	//butcher_results = list(/obj/item/reagent_containers/food/snacks/meat/slab = 2, /obj/item/stack/sheet/sinew/wolf = 4, /obj/item/stack/sheet/sinew/wolf = 4, /obj/item/stack/sheet/bone = 5)
+	loot = list()
+	crusher_loot = /obj/item/crusher_trophy/fang
+
+/mob/living/simple_animal/hostile/asteroid/wolf/alpha/gib()
+	move_force = MOVE_FORCE_DEFAULT
+	move_resist = MOVE_RESIST_DEFAULT
+	pull_force = PULL_FORCE_DEFAULT
+	if(prob(75))
+		new /obj/item/crusher_trophy/fang(loc)
+		visible_message("<span class='warning'>You find an intact fang that looks salvagable.</span>")
+	..()
+
+/obj/item/crusher_trophy/fang
+	name = "battle-stained fang"
+	desc = "A wolf fang, displaying the wear and tear associated with a long and colorful life. Could be attached to a kinetic crusher or used to make a trophy."
+	icon = 'icons/obj/mining_zones/elite_trophies.dmi'
+	icon_state = "fang"
+	denied_type = /obj/item/crusher_trophy/fang
+	var/bleed_stacks_per_hit = 5
+
+/obj/item/crusher_trophy/fang/effect_desc()
+	return "waveform collapse to build up a small stack of bleeding, causing a burst of damage if applied repeatedly."
+
+/obj/item/crusher_trophy/fang/on_mark_detonation(mob/living/M, mob/living/user)
+	if(istype(M) && (M.mob_biotypes & MOB_ORGANIC))
+		var/datum/status_effect/stacking/saw_bleed/bloodletting/B = M.has_status_effect(/datum/status_effect/stacking/saw_bleed/bloodletting)
+		if(!B)
+			M.apply_status_effect(/datum/status_effect/stacking/saw_bleed/bloodletting, bleed_stacks_per_hit)
+		else
+			B.add_stacks(bleed_stacks_per_hit)
+
+/mob/living/simple_animal/hostile/asteroid/wolf/random/Initialize()
+	. = ..()
+	if(prob(15))
+		new /mob/living/simple_animal/hostile/asteroid/wolf/alpha(loc)
+		return INITIALIZE_HINT_QDEL
+
+/mob/living/simple_animal/hostile/asteroid/wolf/wasteland
+	faction = list(FACTION_WASTELAND)
+
+/mob/living/simple_animal/hostile/asteroid/wolf/alpha/wasteland
+	faction = list(FACTION_WASTELAND)
+
+/mob/living/simple_animal/hostile/asteroid/wolf/wasteland/random/Initialize()
+	. = ..()
+	if(prob(15))
+		new /mob/living/simple_animal/hostile/asteroid/wolf/alpha/wasteland(loc)
+		return INITIALIZE_HINT_QDEL
+